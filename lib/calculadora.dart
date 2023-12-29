31ba47820fc44bf7b5e108f7f51d44c88fa5ef42
FloraRosaPupo <florapupo@gmail.com>
2023-12-29 16:52:49 -0300

    appbar

--- lib/calculadora.dart
+++ lib/calculadora.dart
@@ -10,6 +10,50 @@ class Calculadora extends StatefulWidget {
 class _CalculadoraState extends State<Calculadora> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: appBar(),
+      body: Column(children: [
+        Container(
+          //decoration: BoxDecoration(borderRadius: BorderRadius.circular(15)),
+          color: Colors.black,
+          width: MediaQuery.of(context).size.width * 1,
+          height: MediaQuery.of(context).size.height * 0.2,
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width * 1,
+          //height: MediaQuery.of(context).size.height * 0.4,
+          color: Colors.black,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    color: Colors.black,
+                    child: TextButton(
+                      child: Text('AC'),
+                      onPressed: () {},
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        )
+      ]),
+    );
   }
+}
+
+appBar() {
+  return AppBar(
+    title: Text(
+      'Calculadora',
+    ),
+    backgroundColor: Colors.black,
+    titleTextStyle: TextStyle(color: Colors.white, fontSize: 25),
+  );
 }
